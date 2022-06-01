63e699fb514a18752c4a39817f3908780e3e4cc7
bioss7 <fireleny@gmail.com>
2022-06-01 22:01:56 +0700

    upd

--- assets/js/app.js
+++ assets/js/app.js
@@ -93,7 +93,7 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-eval("var hasSubnav = document.querySelectorAll('.nav-menu__link.has-subnav');\nvar hasBody = document.body;\nhasSubnav.forEach(function (item) {\n  return item.addEventListener('click', function (event) {\n    if (hasBody.classList.contains('show-sidebar')) {\n      var $this = event.currentTarget;\n      var subnavMenu = $this.nextElementSibling; // $this.classList.toggle('open');\n\n      if (!$this.classList.contains('open')) {\n        $this.classList.add('open');\n        setTimeout(function () {}, 1);\n      } else {\n        subnavMenu.removeAttribute('style');\n        setTimeout(function () {\n          $this.classList.remove('open');\n        }, 1);\n      }\n    }\n  });\n});\n\n//# sourceURL=webpack:///./src/assets/js/app.js?");
+eval("\n\n//# sourceURL=webpack:///./src/assets/js/app.js?");
 
 /***/ }),
 
@@ -126,18 +126,29 @@ eval("var hasSubnav = document.querySelectorAll('.nav-menu__link.has-subnav');\n
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-eval("var burger = document.getElementById('sidebarToggle');\nvar sidebar = document.getElementById('sidebar');\nvar page = document.getElementById('page');\nvar body = document.body;\nburger.addEventListener('click', function (event) {\n  if (body.classList.contains('show-sidebar')) {\n    closeSidebar();\n  } else {\n    showSidebar();\n  }\n});\n\nfunction showSidebar() {\n  var mask = document.createElement('div');\n  mask.classList.add('page__mask');\n  mask.addEventListener('click', closeSidebar);\n  page.appendChild(mask);\n  body.classList.add('show-sidebar');\n  sidebar.classList.add('sidebar');\n}\n\nfunction closeSidebar() {\n  body.classList.remove('show-sidebar');\n  document.querySelector('.page__mask').remove();\n  sidebar.classList.remove('sidebar');\n}\n\n//# sourceURL=webpack:///./src/assets/js/mobileNav.js?");
+eval("var burger = document.getElementById('sidebarToggle');\nvar sidebar = document.getElementById('sidebar');\nvar page = document.getElementById('page');\nvar body = document.body;\nburger.addEventListener('click', function (event) {\n  if (body.classList.contains('show-sidebar')) {\n    closeSidebar();\n  } else {\n    showSidebar();\n  }\n});\n\nfunction showSidebar() {\n  var mask = document.createElement('div');\n  mask.classList.add('page__mask');\n  mask.addEventListener('click', closeSidebar);\n  page.appendChild(mask);\n  sidebar.classList.add('sidebar');\n  setTimeout(function () {\n    body.classList.add('show-sidebar');\n  }, 200);\n}\n\nfunction closeSidebar() {\n  body.classList.remove('show-sidebar');\n  document.querySelector('.page__mask').remove();\n  setTimeout(function () {\n    sidebar.classList.remove('sidebar');\n  }, 200);\n}\n\n//# sourceURL=webpack:///./src/assets/js/mobileNav.js?");
+
+/***/ }),
+
+/***/ "./src/assets/js/tabs.js":
+/*!*******************************!*\
+  !*** ./src/assets/js/tabs.js ***!
+  \*******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("var tabsBtn = document.querySelectorAll(\".tabs-ui-nav__item\");\nvar tabsItems = document.querySelectorAll(\".tabs-ui__content\");\ntabsBtn.forEach(onTabClick);\n\nfunction onTabClick(item) {\n  item.addEventListener(\"click\", function () {\n    var currentBtn = item;\n    var tabId = currentBtn.getAttribute(\"data-tab\");\n    var currentTab = document.querySelector(tabId);\n\n    if (!currentBtn.classList.contains('active')) {\n      tabsBtn.forEach(function (item) {\n        item.classList.remove('active');\n      });\n      tabsItems.forEach(function (item) {\n        item.classList.remove('active');\n        currentTab.style.opacity = 0;\n      });\n      currentBtn.classList.add('active');\n      currentTab.classList.add('active');\n      setTimeout(function () {\n        currentTab.style.opacity = 1;\n      }, 200);\n    }\n  });\n} // document.querySelector('.tabs-ui-nav__item').click();\n\n//# sourceURL=webpack:///./src/assets/js/tabs.js?");
 
 /***/ }),
 
 /***/ 0:
-/*!**********************************************************************************************************************************!*\
-  !*** multi ./src/assets/js/app.js ./src/assets/js/masonryInit.js ./src/assets/js/mobileDropdown.js ./src/assets/js/mobileNav.js ***!
-  \**********************************************************************************************************************************/
+/*!**********************************************************************************************************************************************************!*\
+  !*** multi ./src/assets/js/app.js ./src/assets/js/masonryInit.js ./src/assets/js/mobileDropdown.js ./src/assets/js/mobileNav.js ./src/assets/js/tabs.js ***!
+  \**********************************************************************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("__webpack_require__(/*! D:\\Programming\\Projects\\UI\\krestonpr-ui\\src\\assets\\js\\app.js */\"./src/assets/js/app.js\");\n__webpack_require__(/*! D:\\Programming\\Projects\\UI\\krestonpr-ui\\src\\assets\\js\\masonryInit.js */\"./src/assets/js/masonryInit.js\");\n__webpack_require__(/*! D:\\Programming\\Projects\\UI\\krestonpr-ui\\src\\assets\\js\\mobileDropdown.js */\"./src/assets/js/mobileDropdown.js\");\nmodule.exports = __webpack_require__(/*! D:\\Programming\\Projects\\UI\\krestonpr-ui\\src\\assets\\js\\mobileNav.js */\"./src/assets/js/mobileNav.js\");\n\n\n//# sourceURL=webpack:///multi_./src/assets/js/app.js_./src/assets/js/masonryInit.js_./src/assets/js/mobileDropdown.js_./src/assets/js/mobileNav.js?");
+eval("__webpack_require__(/*! D:\\Programming\\Projects\\UI\\krestonpr-ui\\src\\assets\\js\\app.js */\"./src/assets/js/app.js\");\n__webpack_require__(/*! D:\\Programming\\Projects\\UI\\krestonpr-ui\\src\\assets\\js\\masonryInit.js */\"./src/assets/js/masonryInit.js\");\n__webpack_require__(/*! D:\\Programming\\Projects\\UI\\krestonpr-ui\\src\\assets\\js\\mobileDropdown.js */\"./src/assets/js/mobileDropdown.js\");\n__webpack_require__(/*! D:\\Programming\\Projects\\UI\\krestonpr-ui\\src\\assets\\js\\mobileNav.js */\"./src/assets/js/mobileNav.js\");\nmodule.exports = __webpack_require__(/*! D:\\Programming\\Projects\\UI\\krestonpr-ui\\src\\assets\\js\\tabs.js */\"./src/assets/js/tabs.js\");\n\n\n//# sourceURL=webpack:///multi_./src/assets/js/app.js_./src/assets/js/masonryInit.js_./src/assets/js/mobileDropdown.js_./src/assets/js/mobileNav.js_./src/assets/js/tabs.js?");
 
 /***/ })
 
